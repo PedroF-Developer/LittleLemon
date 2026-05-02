db252239a4fbd829812ad37564829e6deb7bbeeb
PedroF-Developer <pedro.albanez@hotmail.com>
2026-05-01 22:19:38 -0300

    Setup initial API points and DRF

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, generics
+from .models import Menu, Booking
+from .serializers import MenuItemSerializer, UserSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
 def index(request):
    return render(request, 'index.html', {})
+
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [IsAuthenticated, IsAdminUser]
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
