d6c62c9143597ec8cac04f6a0cfbf84b2982e28f
PedroF-Developer <pedro.albanez@hotmail.com>
2026-05-05 22:07:36 -0300

    Add testing API authentication point and add authentication to other API endpoints

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from rest_framework import viewsets, generics
 from .models import Menu, Booking
 from .serializers import MenuItemSerializer, UserSerializer, BookingSerializer
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from datetime import date
 
 # Create your views here.
 def index(request):
-   return render(request, 'index.html', {})
+   return render(request, 'index.html', {"current_year":date.today().year})
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({"message":"This view is protected"})
 
 class UserViewSet(viewsets.ModelViewSet):
-   queryset = User.objects.all()
-   serializer_class = UserSerializer
-   permission_classes = [IsAuthenticated, IsAdminUser]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
